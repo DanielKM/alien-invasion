ca7065ba92ea282cc16106d59b382eefc17b9efa
Daniel Marshall <38959243+DanielKM@users.noreply.github.com>
2018-06-20 15:26:00 -0400

    Initial Files
    All of my initial files for alien_invasion.

---
+++ alien_invasion/bullet.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 16:37:34 2018
+
+@author: daniel
+"""
+
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = screen
+        
+        # Create a bullet rect at (0,0) and then set the correct position..
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+                                ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+        
+        # Store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+        
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+        
+        
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.speed_factor
+        # Update the rect position.
+        self.rect.y = self.y
+        
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        
+
---
+++ alien_invasion/high_score.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 14:56:33 2018
+
+@author: daniel
+"""
+
+
+def __init__(self, ai_settings):
+        """Saves high scores."""
+        
